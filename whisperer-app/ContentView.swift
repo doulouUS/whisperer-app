f2c1c75131f4d6253355a1b1f9cb2b388f83f7af
doulouUS <louis.douge@gmail.com>
2023-05-29 12:00:54 +0200

    Initial Commit

---
+++ whisperer-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  whisperer-app
+//
+//  Created by Louis DOUGE on 29/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ whisperer-app/whisperer_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisperer_appApp.swift
+//  whisperer-app
+//
+//  Created by Louis DOUGE on 29/5/23.
+//
+
+import SwiftUI
+
+@main
+struct whisperer_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
